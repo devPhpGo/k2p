b6191bd5cf72e60aa83f6d16bac2c4c05454aa55
smile <smile@smiledeAir.lan>
2022-01-11 12:35:42 +0800

    update setting

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,9 +11,27 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 sed -i 's/15744k/32128k/g' target/linux/ramips/image/mt7621.mk
 sed -i 's/"Phicomm K2P";/"Phicomm K2P (32M)";/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/<80000000>/<10000000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/m25p,fast-read;/broken-flash-reset;/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1f60000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+
+# 1.修改默认ip
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 2.修改主机名
+sed -i 's/OpenWrt/K2P(32M)/g' package/base-files/files/bin/config_generate
+
+# 4.修改版本号
+sed -i "s/OpenWrt /0012h build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt（lede） /g" package/lean/default-settings/files/zzz-default-settings
+
+# 5.修改默认主题
+sed -i ' s/luci-theme-bootstrap/luci-theme-argon/g ' feeds/luci/collections/luci/Makefile
+
+# 6.设置ttyd免登录
+#sed -i 's/\/bin\/login/\/bin\/login -f root/' /etc/config/ttyd
+
+# 7.修正连接数（by ベ七秒鱼ベ）
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
